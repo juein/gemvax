9e6f875f013cfa622ce9fbb1f3e710ac119f663f
juein <apocriz@gmail.com>
2020-09-14 14:27:17 +0900

    pc페이지 완료커밋

--- es6/main.js
+++ es6/main.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //리로드시 최상단으로
     window.onbeforeunload = function () {
-        //window.scrollTo(0, 0);
+        window.scrollTo(0, 0);
         //console.clear(); 
     }
 
@@ -22,7 +22,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // default setting
     const defaultSet = () => {
-        console.log('default set');
+        //console.log('default set');
+        gsap.set('.alzheimer__headline--title', {opacity: 0, y: -40});
+        gsap.set('.alzheimer__headline--info', {opacity: 0, y: -40});
     }
 
     // intro-news
@@ -99,8 +101,45 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // intro-news
 
+    // alzheimer 
+    var alzheimerAction = new TimelineLite()
+    .to('.alzheimer__headline--title', 0.6, {opacity: 1, y: 0}, 0.2)
+    .to('.alzheimer__headline--info', 0.6, {opacity: 1, y: 0}, 0.5)
+    .to('.alzheimer__card--item-news.one', 0.5, {opacity: 1}, 0.7)
+    .to('.alzheimer__card--item-news.two', 0.5, {opacity: 1}, 0.8)
+    .to('.alzheimer__card--item-news.three', 0.5, {opacity: 1}, 0.9)
+    .to('.alzheimer__card--item-news.four', 0.5, {opacity: 1}, 1)
 
+    var alzheimerScene = new ScrollMagic.Scene({
+        triggerElement: ".alzheimer",
+        triggerHook: 0.6
+    }).setTween(alzheimerAction).addTo(ctrl);
+    // alzheimer 
+
+    // alzheimer-hope
+    var alzheimerHopeAction =  new TimelineLite()
+    .to('.alzheimer-hope__text-title', 0.8, {opacity: 1, y: 0}, 0.2)
+    .to('.alzheimer-hope__comment-text', 0.6, {opacity: 1}, 0.4)
+    .to('.alzheimer-hope__text-explain', 0.8, {opacity: 1, y: 0}, 0.6)
+    .to('.alzheimer-hope__person', 1, {opacity: 1, x: 0}, 0.5)
+
+    var alzheimerHopeScene = new ScrollMagic.Scene({
+        triggerElement: ".alzheimer-hope",
+        triggerHook: 0.6
+    }).setTween(alzheimerHopeAction).addTo(ctrl);
+    // alzheimer-hope
+
+    
     // conversion
+    var conversionHopeAction =  new TimelineLite()
+    .to('.conversion__content--after-title', 0.6, {opacity: 1}, 0.2)
+    .to('.conversion__content--before-title', 0.6, {opacity: 1}, 0.2)
+
+    var conversionHopeScene = new ScrollMagic.Scene({
+        triggerElement: ".conversion",
+        triggerHook: 0.6
+    }).setTween(conversionHopeAction).addTo(ctrl);
+
     $('.drag__cover--btn').hover(function(){
         gsap.to('.drag__cover--btn', 0.5, {opacity: 0, delay: 0.1});
         gsap.to('.drag__cover', 0.8, {scaleX: 0, delay: 0.4});
@@ -162,31 +201,183 @@ window.addEventListener('DOMContentLoaded', () => {
     
 
     // drug-spec
-    
-    //let drguMg = (window.innerHeight - 880) / 2;
-    //let drugOffset = 1920 - drguMg;
+    var drugSpecAction =  new TimelineLite()
+    .to('.drug-spec__panel--title', 0.6, {opacity: 1, y: 0}, 0.2)
+    .to('.drug-spec__panel--cover', 0.6, {opacity: 1}, 0.4)
 
-    console.log('drugOffset = ' + drugOffset);
+    var drugSpecScene = new ScrollMagic.Scene({
+        triggerElement: ".drug-spec",
+        triggerHook: 0.6
+    }).setTween(drugSpecAction).addTo(ctrl);
+
+    const drugSpecStep = {
+        step1(){
+            $('.drug-spec__panel--inner').removeClass('active');
+            $('.drug-spec__panel--inner.step_1').addClass('active');
+            gsap.to('.drug-spec__background', 0.6, {opacity: 1});
+        },
+        step2(){
+            $('.drug-spec__panel--inner').removeClass('active');
+            $('.drug-spec__panel--inner.step_2').addClass('active');
+            gsap.to('.drug-spec__background', 0.6, {opacity: 0});
+        },
+        step3(){
+            $('.drug-spec__panel--inner').removeClass('active');
+            $('.drug-spec__panel--inner.step_3').addClass('active');
+            gsap.to('.drug-spec__background', 0.6, {opacity: 0});
+        }
+    }
+
+    //drug-swite--2
+    var drugSwite2 = new Swiper('.drug-swite--2', {
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        //autoplay: { delay: 1000, },
+        speed: 800,
+        allowClick : false,
+        allowTouchMove: false,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        on: {
+            slideChangeTransitionStart: function(){
+                var idx = this.realIndex + 1;
+                console.log('idx = ' + idx);
+                $('.drug-spec__panel--inner .step_2 .explain-text span').removeClass('active');
+                $('.drug-spec__panel--inner .step_2 .explain-text span.idx-'+idx).addClass('active');
+            }
+        }
+    });
+
+    var drugSwite3 = new Swiper('.drug-swite--3', {
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        //autoplay: { delay: 500, },
+        speed: 500,
+        allowClick : false,
+        allowTouchMove: false,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        on: {
+            slideChangeTransitionStart: function(){
+                var idx = this.realIndex + 1;
+                console.log('idx = ' + idx);
+                $('.drug-spec__panel--inner .step_3 .explain-text span').removeClass('active');
+                $('.drug-spec__panel--inner .step_3 .explain-text span.idx-'+idx).addClass('active');
+            }
+        }
+    });
+
     var drugSpecScene = new ScrollMagic.Scene({
         triggerElement: ".drug-spec__pin",
         triggerHook: "onLeave",
-        duration: "3000",
-        //offset: -200
-        offset: drugOffset
+        duration: "2500",
+        offset : -45
+        ////offset: -200
+        //offset: drugOffset
       }).setPin(".drug-spec__pin")
       .addTo(ctrl).on("progress", function (e) {
 
-        let test = e.progress.toFixed(1);
-            console.log(test)
-       
-      })
-      .addIndicators({
-        colorTrigger: "red", //트리거 팁 색상
-        colorStart: "red", //스타트 팁 색상
-        colorEnd: "red", //종료 팁 색상
-        indent: 40 //우측 스크롤바부터 얼마나 떨어뜨릴지
+        let drugSpecStepPoint = e.progress.toFixed(1);
+            console.log(drugSpecStepPoint)
+
+
+            if(drugSpecStepPoint <= 0.3){
+                
+                drugSpecStep.step1();
+            }else if(drugSpecStepPoint <= 0.6){
+                
+                drugSpecStep.step2();
+            }else {
+                
+                drugSpecStep.step3();
+            }
+            
       });
+      //.addIndicators({
+      //  colorTrigger: "red", //트리거 팁 색상
+      //  colorStart: "red", //스타트 팁 색상
+      //  colorEnd: "red", //종료 팁 색상
+      //  indent: 40 //우측 스크롤바부터 얼마나 떨어뜨릴지
+      //});
     // drug-spec
+
+
+    // meterial
+    var meterialAction =  new TimelineLite()
+    .to('.meterial__panel--title', 0.6, {opacity: 1, y: 0}, 0.2)
+
+    var meterialScene = new ScrollMagic.Scene({
+        triggerElement: ".meterial",
+        triggerHook: 0.6
+    }).setTween(meterialAction).addTo(ctrl);
+
+    let meterialHover;
+    $(".meterial__panel--inner-list-item").hover(function(){
+        if( $(this).hasClass('active') ) return false;
+        meterialHover = $(this).attr('class').replace('meterial__panel--inner-list-item list','');
+        
+        $(".meterial__panel--inner-list-item").removeClass('active');
+        $(".meterial__panel--inner-list-item.list"+meterialHover).addClass('active');
+
+
+    }, function(){
+        console.log('leave');
+        $(".meterial__panel--inner-list-item").removeClass('active');
+    });
+    // meterial
+
+
+    // gv-1001
+    var gv1001Action =  new TimelineLite()
+    .to('.gv-1001__panel--title', 0.6, {opacity: 1, y: 0}, 0.2)
+    .to('.gv-1001__panel--content-comment .quotation-left', 0.6, {opacity: 1}, 0.4)
+    .to('.gv-1001__panel--content-comment .quotation-right', 0.6, {opacity: 1}, 0.4)
+    .to('.gv-1001__panel--content-text', 1, {opacity: 1}, 0.6)
+    .to('.gv-1001__panel--content-detail', 0.6, {opacity: 1, y: 0}, 1)
+    .to('.gv-1001__panel--content-image', 1, {opacity: 1}, 1.2)
+
+    var gv1001Scene = new ScrollMagic.Scene({
+        triggerElement: ".gv-1001",
+        triggerHook: 0.4
+    }).setTween(gv1001Action).addTo(ctrl);
+    // gv-1001
+
+
+    // meterial-hope
+    var meterialhopeAction =  new TimelineLite()
+    .to('.meterial-hope__panel--title', 0.6, {opacity: 1, y: 0}, 0.2)
+    .to('.meterial-hope__panel--table', 1, {opacity: 1}, 0.4)
+    .to('.table__background', 1.2, {opacity: 1}, 0.6)
+    .to('.meterial-hope__panel--explain', 0.6, {opacity: 1, y: 0}, 0.6)
+
+    var tableBgRotate = new TimelineLite({paused: true, repeat: -1})
+    .to('.table__background li:nth-of-type(1)', 2, {background: '#1e00ff'}, 0)
+    .to('.table__background li:nth-of-type(2)', 2, {background: '#1e00ff'}, 2)
+    .to('.table__background li:nth-of-type(1)', 0.5, {background: '#0c0066'}, 2)
+    .to('.table__background li:nth-of-type(3)', 2, {background: '#1e00ff'}, 4)
+    .to('.table__background li:nth-of-type(2)', 0.5, {background: '#0c0066'}, 4)
+
+    var meterialhopeScene = new ScrollMagic.Scene({
+        triggerElement: ".meterial-hope",
+        triggerHook: 0.6
+    }).setTween(meterialhopeAction).addTo(ctrl)
+    .on('enter leave', function(e){
+        //console.log(e);
+        if(e.state == 'BEFORE'){
+            tableBgRotate.pause();  
+        }else {
+            tableBgRotate.play();  
+        }
+    });
+    // meterial-hope
 
     // alzheimer__card
     let hoverItem;
@@ -204,6 +395,26 @@ window.addEventListener('DOMContentLoaded', () => {
     // alzheimer__card
 
 
+    // outro
+    var outroAction =  new TimelineLite()
+    .to('.outro__panel--title', 0.6, {opacity: 1, y: 0}, 0.2)
+    .to('.outro__panel--content-text', 0.6, {opacity: 1, y: 0}, 0.4)
+    .to('.outro__panel--content-strong', 0.6, {opacity: 1, y: 0}, 0.6)
+    .to('.outro__panel--content-person', 1, {opacity: 1}, 1)
+    .to('.outro__panel--content-comment', 1, {opacity: 1}, 1.4)
+
+    var outroScene = new ScrollMagic.Scene({
+        triggerElement: ".outro",
+        triggerHook: 0.1,
+        offset : 400
+    }).setTween(outroAction).addTo(ctrl);
+    //.addIndicators({
+    //    colorTrigger: "white", //트리거 팁 색상
+    //    colorStart: "white", //스타트 팁 색상
+    //    colorEnd: "white", //종료 팁 색상
+    //    indent: 40 //우측 스크롤바부터 얼마나 떨어뜨릴지
+    //  });
+    // outro
 
 
     
